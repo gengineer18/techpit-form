9949363b8ce29bed57dfa4c2c24b2afd60208b13
gengineer18 <51533184+gengineer18@users.noreply.github.com>
2020-06-05 09:14:52 +0900

    1-4 (#3)

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { TextField } from '@material-ui/core'
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from '@material-ui/core'
 
+// 型定義
 import { RootState } from "../domain/entity/rootState"
 import { Profile } from "../domain/entity/profile"
-import { profileActions } from "../store/profile/actions"
+import { Gender } from '../domain/entity/gender'
 
+// 定数
 import { PROFILE } from '../domain/services/profile'
 
+// action
+import { profileActions } from "../store/profile/actions"
+
+// css
 import useStyles from './styles'
 
 export const Basic = () => {
@@ -37,6 +50,24 @@ export const Basic = () => {
         value={profile.description}
         onChange={e => handleChange({description: e.target.value})}
       />
+      <FormControl className={classes.formField}>
+        <FormLabel>{PROFILE.GENDER}</FormLabel>
+        <RadioGroup
+          value={profile.gender}
+          onChange={e => handleChange({gender: e.target.value as Gender})}
+        >
+          <FormControlLabel
+            value="male"
+            label="男性"
+            control={<Radio color="primary" />}
+          />
+          <FormControlLabel
+            value="female"
+            label="女性"
+            control={<Radio color="primary" />}
+          />
+        </RadioGroup>
+      </FormControl>
     </>
   )
 }
